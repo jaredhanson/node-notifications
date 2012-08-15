232585aaeb685e4d8348feb281a41abf2fd2eddb
Jared Hanson <jaredhanson@gmail.com>
2012-08-15 00:02:08 -0700

    Wrap with UMD.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('./lib/notifications/index');
+(function(root, factory) {
+  if (typeof exports === 'object') {
+    // Node.js
+    factory(exports, module, require('./lib/notifications/index'));
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(['exports', 'module', './lib/notifications/index'], factory);
+  }
+}(this, function(exports, module, notifications) {
+  exports = module.exports = notifications;
+}));
